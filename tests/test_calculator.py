4b16cf8b1a80017bde9a2d5f1db313b9a1b5a52c
Travis Logue <91381575+SecTraversl@users.noreply.github.com>
2023-11-13 20:08:40 +0000

    adding message to git commit statement

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -2,6 +2,7 @@ from src.calculator import add, sub, mul, div
 
 def test_add():
     assert add(1,1) == 2  # branch "test3" set back to `assert add(1,1) == 2`
+    # alright looks good, let's do a git commit * -m 'adding message to git commit statement'
 
 def test_sub():
     assert sub(1,1) == 0
